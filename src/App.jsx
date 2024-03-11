5b66ca3fc68537a0c0397d18dd5eb25a0a3fd5c5
Saad Srabon <saadsrabon2@gmail.com>
2024-03-11 21:43:12 +0600

    2024-03-11 21:43:12 - footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { Route, Routes } from "react-router-dom"
 import Header from "./components/common/Header"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Footer from "./components/common/Footer"
 
 
 function App() {
@@ -7,6 +12,12 @@ function App() {
   return (
     <>
      <Header/>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+     </Routes>
+     <Footer/>
     </>
   )
 }
