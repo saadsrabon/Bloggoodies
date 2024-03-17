27ccbb9af9c95b11cde0dbe924f03adf363fde7a
Saad Srabon <saadsrabon2@gmail.com>
2024-03-17 21:39:31 +0600

    2024-03-17 21:39:31 - comment show hide option added

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,12 +1,17 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import comment from "../assets/icons/comment.svg"
+import favourite from "../assets/icons/heart.svg"
+import like from "../assets/icons/like.svg"
+import favourited from '../assets/icons/heart-filled.svg'
+import { useAuth } from './../hooks/useAuth';
 
-
-const SingleBlog = ({}) => {
+const SingleBlog = () => {
     const url =window.location.pathname.split("/")[2]
-    console.log(url)
+ 
 const [blog,setBlog]=useState({})
-console.log(blog)
+ const {currentUserId}=useAuth()
+ console.log(currentUserId)
     useEffect(()=>{
         const fetchSingleBlog =async(id)=>{
                const response =await axios.get(`http://localhost:3000/blogs/${id}`)
@@ -16,6 +21,7 @@ console.log(blog)
         fetchSingleBlog(url)
     },[url])
   return (
+    <>
     <main>
     {/* <!-- Begin Blogs --> */}
     <section>
@@ -35,37 +41,14 @@ console.log(blog)
 
         {/* <!-- Tags --> */}
         <ul className="tags">
-          <li>JavaScript</li>
-          <li>Node</li>
-          <li>React</li>
-          <li>Next</li>
+          {blog?.tags?.split(',').map((tag,index)=>(
+            <li key={index} className="bg-indigo-600 text-white px-4 py-1 rounded-md text-sm">{tag}</li>
+          ))}
         </ul>
 
         {/* <!-- Content --> */}
         <div className="mx-auto w-full md:w-10/12 text-slate-300 text-base md:text-lg leading-8 py-2 !text-left">
-          Today I was mob programming with Square's Mobile & Performance Reliability team and we toyed with an
-          interesting idea. Our codebase has classNamees that represent screens a user can navigate to. These classNamees are
-          defined in modules, and these modules have an owner team defined. When navigating to a screen, we wanted to
-          have the owner team information available, at runtime. We created a build tool that looks at about 1000
-          Screen classNamees, determines the owner team, and generates a className to do the lookup at runtime. The generated
-          code looked like this:
-
-          <br />
-
-          mapOf(vararg pairs: Pair) is a nice utility to create a map (more specifically, a LinkedHashMap) but using
-          that syntax leads to the creation of a temporary vararg array of size 1000, as well as 1000 temporary Pair
-          instances. Memory hoarding Let's look at the retained size of the map we just created: ~30 characters per
-          className name * 2 bytes per character = 60 bytes per entry Each entry is stored as a LinkedHashMapEntry which
-          adds 2 references to HashMap.Node which itself holds 3 references and 1 int. On a 64bit VM that's 5
-          references * 8 bytes, plus 4 bytes for the int: 44 bytes per entry. So for the entries alone we're at (60 +
-          44) * 1000 = 104 KB. The default load factor is 75%, which means the size of the array backing the hashmap
-          must always be at least 25% greater than the number of entries. And the array size has to be a factor of 2.
-          So, for 1000 entries, that's an object array of size 2048: 2048 * 8 = 16,314 bytes. The total retained size
-          of the map is ~120 KB. Can we do better? Could we make it... 0?
-          <h2 className="font-bold text-3xl mt-4">100% code-based map</h2>
-          What if we generate code that returns the right team for a given screen, instead of creating a map? Since we
-          know the full list of screen classNamees, we can check ahead of time whether there's any hashcode conflict, and
-          if not, we can generate code that directly
+          {blog?.content}
         </div>
       </div>
     </section>
@@ -75,6 +58,7 @@ console.log(blog)
     <section id="comments">
       <div className="mx-auto w-full md:w-10/12 container">
         <h2 className="text-3xl font-bold my-8">Comments (3)</h2>
+        {currentUserId &&
         <div className="flex items -center space-x-4">
           <div className="avater-img bg-indigo-600 text-white">
             <span className="">S</span>
@@ -92,7 +76,7 @@ console.log(blog)
               </button>
             </div>
           </div>
-        </div>
+        </div>}
 
         {/* <!-- Comment One --> */}
         <div className="flex items-start space-x-4 my-8">
@@ -150,6 +134,27 @@ console.log(blog)
       </div>
     </section>
   </main>
+
+  <div className="floating-action">
+      <ul className="floating-action-menus">
+        <li>
+          <img src={like} alt="like" />
+          <span>10</span>
+        </li>
+
+        <li>
+          {/* <!-- There is heart-filled.svg in the icons folder --> */}
+          <img src={favourite} alt="Favourite" />
+        </li>
+        <a href="#comments">
+          <li>
+            <img src={comment} alt="Comments" />
+            <span>3</span>
+          </li>
+        </a>
+      </ul>
+    </div>
+  </>
   )
 }
 
